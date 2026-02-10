6bf8cd1521f5f5f59c8db83ae3dd002304491465
Jakub Tuzar <jakub@tuzar.me>
2026-02-10 11:31:02 +0100

    lazy code claude gone

--- nvim/.config/nvim/lua/plugins/99.lua
+++ nvim/.config/nvim/lua/plugins/99.lua
@@ -60,17 +60,6 @@ return {
       model = "openai/gpt-5.2"
     })
 
-    -- Create your own short cuts for the different types of actions
-    vim.keymap.set("n", "<leader>9f", function()
-      _99.fill_in_function()
-    end)
-    -- take extra note that i have visual selection only in v mode
-    -- technically whatever your last visual selection is, will be used
-    -- so i have this set to visual mode so i dont screw up and use an
-    -- old visual selection
-    --
-    -- likely ill add a mode check and assert on required visual mode
-    -- so just prepare for it now
     vim.keymap.set("v", "<leader>9v", function()
       _99.visual()
     end)
